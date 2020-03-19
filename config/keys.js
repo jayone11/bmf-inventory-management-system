cf420d87a0830966cfd4e5f3f9b17ace8202f1ee
Jeff B <neustellar@gmail.com>
2020-03-19 12:50:21 -0700

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // mongoURI: "mongodb+srv://efren45marin:Wander89%21@cluster0-3jmno.mongodb.net/test?retryWrites=true&w=majority",
+  mongoURI: "mongodb://rishipr:teams123@ds155815.mlab.com:55815/teams",
+  secretOrKey: "secret"
+};
